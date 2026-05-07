63df97bd7e98b8dd943db0e3a3fcf7087084e49a
XYCert <107841184+xycertbsi@users.noreply.github.com>
2026-05-07 16:24:16 +0200

    Update get_date.py

--- scripts/get_date.py
+++ scripts/get_date.py
@@ -4,8 +4,8 @@ import datetime
 tts = pyttsx3.init()
 
 def run():
-    now = datetime.datetime.now()  # Lekérjük az aktuális dátumot és időt
-    current_date = now.strftime("%Y-%m-%d")  # Formázott dátum
-    print("Jelenlegi dátum:", current_date)
-    tts.say("Jelenlegi dátum:" + current_date)
+    now = datetime.datetime.now()
+    current_date = now.strftime("%Y-%m-%d")
+    print("Current Date:", current_date)
+    tts.say("Current Date:" + current_date)
     tts.runAndWait()
